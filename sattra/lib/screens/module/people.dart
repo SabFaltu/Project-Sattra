efb48b5226b3bc29d21fb3b3d3a85cb0b937cdd2
skshm <eminate894567@protonmail.com>
2024-02-19 07:21:48 +0530

    Fixed Overflow

--- sattra/lib/screens/module/people.dart
+++ sattra/lib/screens/module/people.dart
@@ -26,19 +26,18 @@ class _PatientWidgetState extends State<PatientWidget> {
                   child: AnimatedContainer(
                     duration: const Duration(milliseconds: 100),
                     curve: Curves.easeIn,
-                    width: 250,
-                    height: 180,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        Row(
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsetsDirectional.fromSTEB(
-                                  0, 4, 4, 0),
-                              child: ClipRRect(
+                    width: 350, // Consider making this dynamic if needed
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min, // Changed to min
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Row(
+                            mainAxisSize: MainAxisSize.max,
+                            crossAxisAlignment: CrossAxisAlignment.start, // Added to align items at the start
+                            children: [
+                              ClipRRect(
                                 borderRadius: BorderRadius.circular(6),
                                 child: Image.network(
                                   'https://images.unsplash.com/photo-1681668385364-af51bce51c18?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
@@ -47,81 +46,85 @@ class _PatientWidgetState extends State<PatientWidget> {
                                   fit: BoxFit.cover,
                                 ),
                               ),
-                            ),
-                            const Text(
-                              'Devinder\nSingh',
-                              textAlign: TextAlign.start,
-                              maxLines: 2,
-                              style: TextStyle(fontSize: 28),
-                            ),
-                          ],
-                        ),
-                        Padding(
-                          padding:
-                              const EdgeInsetsDirectional.fromSTEB(0, 6, 0, 0),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.max,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: const <Widget>[
-                              Text.rich(
-                                TextSpan(
-                                  children: [
-                                    TextSpan(
-                                      text: 'Age: ',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.black,
-                                        fontSize: 17,
-                                      ),
-                                    ),
-                                    TextSpan(
-                                      text: 'Hello World ',
-                                      style: TextStyle(
-                                        color: Colors.black,
-                                      ),
-                                    )
-                                  ],
-                                  style: TextStyle(fontSize: 15),
-                                ),
-                              ),
-                              Text.rich(
-                                TextSpan(
-                                  children: [
-                                    TextSpan(
-                                      text: 'Gender: ',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    TextSpan(
-                                      text: 'Hello World ',
-                                      style: TextStyle(fontSize: 15),
-                                    )
-                                  ],
-                                  style: TextStyle(fontSize: 17),
-                                ),
-                              ),
-                              Text.rich(
-                                TextSpan(
-                                  children: [
-                                    TextSpan(
-                                      text: 'Last Checkup: ',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    TextSpan(
-                                      text: 'Hello World ',
-                                      style: TextStyle(fontSize: 15),
-                                    )
-                                  ],
-                                  style: TextStyle(fontSize: 17),
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(left: 8.0),
+                                  child: Text(
+                                    'Devinder\nSingh',
+                                    textAlign: TextAlign.start,
+                                    maxLines: 2,
+                                    style: TextStyle(fontSize: 28),
+                                  ),
                                 ),
                               ),
                             ],
                           ),
-                        ),
-                      ],
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8.0),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const <Widget>[
+                                Text.rich(
+                                  TextSpan(
+                                    children: [
+                                      TextSpan(
+                                        text: 'Age: ',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          // color: Colors.black,
+                                          fontSize: 17,
+                                        ),
+                                      ),
+                                      TextSpan(
+                                        text: '42',
+                                        style: TextStyle(
+                                          // color: Colors.black,
+                                        ),
+                                      )
+                                    ],
+                                    style: TextStyle(fontSize: 15),
+                                  ),
+                                ),
+                                Text.rich(
+                                  TextSpan(
+                                    children: [
+                                      TextSpan(
+                                        text: 'Gender: ',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      TextSpan(
+                                        text: 'Male',
+                                        style: TextStyle(fontSize: 15),
+                                      )
+                                    ],
+                                    style: TextStyle(fontSize: 17),
+                                  ),
+                                ),
+                                Text.rich(
+                                  TextSpan(
+                                    children: [
+                                      TextSpan(
+                                        text: 'Last Checkup: ',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      TextSpan(
+                                        text: '12 Jan 2023',
+                                        style: TextStyle(fontSize: 15),
+                                      )
+                                    ],
+                                    style: TextStyle(fontSize: 17),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
--- sattra/lib/screens/view/patient.dart
+++ sattra/lib/screens/view/patient.dart
@@ -53,8 +53,14 @@ class _PatientsPageState extends State<PatientsPage> with PageMixin {
   @override
   Widget build(BuildContext context) {
     assert(debugCheckHasFluentTheme(context));
+
     double screenWidth = MediaQuery.of(context).size.width;
-    int crossAxisCounted = (screenWidth ~/ 250).clamp(1, 6);
+
+    // Dynamically calculate the number of columns based on screen width
+    int crossAxisCounted = (screenWidth ~/ 320);
+
+    // Adjust childAspectRatio based on your design needs
+    double childAspectRatio = 0.85; // Adjust this value as needed
 
     return ScaffoldPage.scrollable(
       header: PageHeader(
@@ -69,14 +75,16 @@ class _PatientsPageState extends State<PatientsPage> with PageMixin {
       children: [
         GridView.builder(
           shrinkWrap: true,
-          itemCount:
-              15, // Change this to the number of cards you want to display
+
+          itemCount: 15, // Adjust based on your data
+
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: crossAxisCounted,
             crossAxisSpacing: 8.0,
-            mainAxisSpacing: 9.0,
-            //childAspectRatio: 1.3,
+            mainAxisSpacing: 4.0,
+            // childAspectRatio: childAspectRatio,
           ),
+
           itemBuilder: (BuildContext context, int index) {
             return PatientWidget();
           },
